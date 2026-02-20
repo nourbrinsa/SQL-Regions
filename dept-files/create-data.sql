6b4ec949b7f44dca8a9f44cc543967486ecfdec4
Nour Ben Rejeb <nour.ben_rejeb@insa-cvl.fr>
2026-02-20 16:57:52 +0100

    Initial commit of my TD noté SQL

---
+++ dept-files/create-data.sql
@@ -0,0 +1,45 @@
+.mode box
+PRAGMA foreign_keys = on;
+
+drop table if exists zus;
+drop table if exists voisins;
+drop table if exists departements;
+drop table if exists regions;
+
+
+create table regions (
+	rid char(5) primary key,
+	nom varchar(60),
+	chefLieu varchar(60)
+);
+
+create table departements (
+	code numeric(3) primary key,
+	nom varchar(60) UNIQUE,
+	prefecture varchar(60),
+	rid char(5),
+	foreign key (rid) references regions
+);
+
+create table voisins (
+	rid1 char(5),
+	rid2 char(5),
+	primary key (rid1,rid2),
+	foreign key (rid1) references regions,
+	foreign key (rid2) references regions
+);
+
+create table zus (
+	departement varchar(60),
+	commune varchar(60),
+	quartier varchar(60),
+	primary key(commune,quartier),
+	foreign key (departement) references departements(nom)
+);
+
+.separator ','
+.import 'regions.csv' regions
+.import 'departements.csv' departements
+.import 'voisins.csv' voisins
+.separator ';'
+.import 'zus.csv' zus
---
+++ dept-files/requetes.sql
@@ -0,0 +1,113 @@
+--q2
+
+select code,nom 
+from departements 
+where prefecture = 'Bourges';
+
+--q3
+
+select code,d.nom,prefecture,r.nom 
+from regions r, departements d 
+where r.rid = d.rid;
+
+--q4
+
+select r.nom,chefLieu,code,d.nom,prefecture 
+from regions r,departements d 
+where r.rid = d.rid 
+order by r.nom asc;
+
+--q5
+
+select code,d.nom,prefecture 
+from regions r, departements d 
+where r.rid = d.rid and r.nom = 'Centre-Val de Loire';
+
+--q6
+
+select count(*) as nb 
+from voisins;
+
+--q7
+
+drop view if exists voisinsSym; 
+create view voisinsSym as
+select rid1, rid2 from voisins
+UNION
+select rid2 as rid1, rid1 as rid2 from voisins; 
+select * from voisinsSym;
+
+--q7 bis
+select count(*) as nb_total_voisins
+from voisinsSym;
+--Le résultat (46)  est le double du résultat précédent (23) ce qui est coherent parce que pour chaque ville on a ajouté son sym
+
+--q8
+drop view if exists voisinsSymNoms; 
+create view voisinsSymNoms as select r1.nom as Region1, r2.nom as Region2 
+from regions r1,regions r2,voisinsSym v 
+where r1.rid=v.rid1 and r2.rid=v.rid2; 
+select * from voisinsSymNoms;
+
+--q8 bis
+select r.nom as region, count(v.Region2) as nb_voisins
+from regions r
+left join voisinsSymNoms v on v.Region1 = r.nom
+group by r.nom
+order by nb_voisins desc;
+
+--q11
+drop view if exists zus_pref; 
+create view zus_pref as 
+	select departement,commune,quartier 
+	from zus,departements 
+	where departement = nom and commune like '%'||prefecture||'%'; 
+select * from zus_pref;
+
+--q11 bis
+select count(*) as nb,'Total' as type
+from zus
+UNION ALL
+select coun(*) as nb, 'Oui' as type
+from zus_pref
+UNION ALL
+select count(*) as nb, 'Non' as type
+from zus
+where (departement,commune,quartier) not in (select departement,commune,quartier from zus_pref);
+
+--q12
+select d.code, d.nom as departement, r.nom as region, COUNT(z.quartier) as nb_zus
+from departements d
+join regions r on r.rid = d.rid
+left join zus z on z.departement = d.nom
+group by d.code, d.nom, r.nom
+order by nb_zus DESC, d.nom ASC;
+
+--q13
+select r.nom, count(*) as nb_zus_reg 
+from zus z, departements d, regions r 
+where z.departement = d.nom and d.rid = r.rid 
+group by r.rid 
+order by nb_zus_reg desc;
+
+--q14V1
+select r.nom
+from regions r
+where not exists (
+	select * 
+	from departements d 
+	where d.rid = r.rid and not exists (
+      		select *
+      		from zus z
+      		where z.departement = d.nom
+    	)
+);
+
+--q14V2
+select r.nom
+from regions r
+join departements d on d.rid = r.rid
+left join zus z on z.departement = d.nom
+group by r.rid, r.nom
+having COUNT(DISTINCT d.code)
+     = COUNT(DISTINCT CASE WHEN z.quartier IS NOT NULL THEN d.code END);
